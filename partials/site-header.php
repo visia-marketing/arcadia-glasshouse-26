a86576507d55a02074b9aeb4d70197ba5318ba5b
Randal Pope <randalpope@mac.mynetworksettings.com>
2026-02-25 15:24:18 -0500

    updated top header icons

--- partials/site-header.php
+++ partials/site-header.php
@@ -2,7 +2,7 @@
 <div class="top-header">
 	<div class="uk-container uk-container-xlarge">
 		<div class="uk-width-1-1">
-      <div class="top-header-flex">
+      <div class="top-header-flex uk-margin-small-top uk-margin-small-bottom">
         <?php
           wp_nav_menu(['theme_location' => 'top_navigation', 'depth' => 1, 'menu_class' => 'top-header-navigation top-header-navigation-right']); 
         ?>
